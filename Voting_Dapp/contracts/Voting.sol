0c63cb3ca94f237fe0baa7d2695b73ca91b84958
Akhilesh Pratap Singh <apsingh@dminc.com>
2018-02-23 20:09:04 +0530

    Added readme file with instruction

--- Voting_Dapp/contracts/Voting.sol
+++ Voting_Dapp/contracts/Voting.sol
@@ -4,23 +4,23 @@ contract Voting {
     mapping (bytes32=>uint8) public votesReceived;
     bytes32[] public candidateList;
  
-    function Voting(bytes32[] candidateNames){
+    function Voting(bytes32[] candidateNames) {
         candidateList=candidateNames;
     }
  
     function totalVotesFor(bytes32 candidate) returns (uint8) {
-        if (validCandidate(candidate) == false) throw;
+        if (validCandidate(candidate) == false) 
+        revert();
         return votesReceived[candidate];
     }
  
-    function voteForCandidate(bytes32 candidate) 
-    {
-        if (validCandidate(candidate) == false) throw;
+    function voteForCandidate(bytes32 candidate) {
+        if (validCandidate(candidate) == false) 
+        revert();
         votesReceived[candidate] += 1;
     }
  
-    function validCandidate(bytes32 candidate) returns (bool) 
-    {
+    function validCandidate(bytes32 candidate) returns (bool) {
         for(uint i = 0; i < candidateList.length; i++) {
         if (candidateList[i] == candidate) {
             return true;
